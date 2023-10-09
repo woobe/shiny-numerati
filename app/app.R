c15f45fcffea9521cb6c2bc6812fdaabc54f5cc4
Jo-fai Chow <jofai.chow@gmail.com>
2023-10-09 20:57:40 +0200

    Added KPI chart note

--- app/app.R
+++ app/app.R
@@ -1150,7 +1150,7 @@ server <- function(input, output) {
   })
   
   output$text_performance_chart_title <- renderText({
-    if (nrow(react_d_filter()) >= 1) "KPI Chart" else " "
+    if (nrow(react_d_filter()) >= 1) "KPI Chart (Remember to Click the 'Refresh' Button ↑↑↑)" else " "
   })
   
   output$text_performance_chart_data <- renderText({
